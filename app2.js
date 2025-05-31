3e37125159ec29416453f38b88016d63627cf97c
Vinicius Muniz Costa <viniciusmuniz0313@gmail.com>
2025-05-31 08:30:21 -0300

    Início do HTML e do JS

---
+++ app2.js
@@ -0,0 +1,13 @@
+
+const onboarding = [
+    {
+        idade: null,
+        tarefas_diarias: null,
+        perfil: [
+            {"id": 1, "text": "produtiva"}
+        ],
+        objetivo: [
+            {"id": "a", "text":"Evitar a procrastinação"}
+        ]
+    }
+]
