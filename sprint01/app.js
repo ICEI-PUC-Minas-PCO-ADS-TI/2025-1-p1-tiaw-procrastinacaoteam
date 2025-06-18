1cb18944020d3cc824be4dffa70f5112c141695d
Vinicius Muniz Costa <vinicius.costa.1434010@sga.pucminas.br>
2025-06-17 23:26:18 -0300

    Add files via upload

--- sprint01/app.js
+++ sprint01/app.js
@@ -33,3 +33,67 @@ const informacoes = [
     }
 ]
 
+        //Interação de RETROSPECTO.HTML
+
+        window.onload = function () {
+            let tela = document.getElementById('tela')
+
+        for(let i = 0; i < informacoes.length; i++) {
+            dado = informacoes[i]  //se refere ao meu JSON 
+
+            tela.innerHTML += `<div id='dados'> <div class='dadosjs'>PONTOS: ${dado.pontos} </div><br>
+            <br>
+            <div class='dadosjs'>SEMANA ${dado.semanas} </div> <br>
+            <br>
+            <div class='dadosjs'><a href="detalhes.html?id=${dado.tarefas[0].id}">TAREFAS CONCLUÍDAS: ${dado.tarefas[0].itens.length}</a> </div> <br>  
+            <br>
+            <div class='dadosjs'><a href="detalhes.html?id=${dado.tarefas[1].id}">TAREFAS PENDENTES: ${dado.tarefas[1].itens.length}</a> </div> <br>
+            <br> </div>`
+             //os dois arrays do meu json
+            
+        }
+
+        let ultimo = informacoes[informacoes.length - 1];
+        let total = ultimo.tarefas[0].itens.length + ultimo.tarefas[1].itens.length;
+        let percentual = (ultimo.tarefas[0].itens.length / total) * 100;
+
+
+        tela.innerHTML += "<p id='p-grafico'>Porcentagem: " + percentual + "% das tarefas concluídas</p>"
+        }
+        
+
+
+        
+        
+        //Interação da página DETALHES.HTML
+        let params = new URLSearchParams(location.search) 
+        let id = params.get('id')
+        let exibicao1 = document.getElementById('exibicao')
+
+        const tarefas = informacoes[0].tarefas  //acessa o array tarefas do meu JSON
+
+        const exibicao = tarefas.find(function (t) {return t.id == id})
+        
+
+        if (exibicao) {
+            exibicao1.innerHTML += `<header class='cabecalho'><h1>Tarefas ${exibicao.tipo}</h1></header>`;
+            exibicao.itens.forEach(item => {  //foreach vai passar pelo meus itens
+                exibicao1.innerHTML += `<div id='tarefas-exibidas'><li>${item.tarefa}</li></div> <br>`
+            });
+
+        }
+        else {
+            exibicao1.innerHTML = "URL inválida. Tente novamente..."
+        }
+        
+        
+  
+
+
+ 
+
+
+
+
+
+
